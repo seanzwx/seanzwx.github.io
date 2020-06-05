d5150c1bb31c7be5c3d5b5102027187ceb496408
sean <sean_zwx@163.com>
2020-06-05 18:18:39 +0800

    sdfsdf

--- app/tincat/game/spider-games58.js
+++ app/tincat/game/spider-games58.js
@@ -3,36 +3,43 @@
  */
 (function()
 {
-    var gameList = {
-        trending: [],
-        gameArray: []
-    };
+    var gameList = [];
 
     // 热门游戏
+    var trendingGame = {
+        title: "Trending Games",
+        itemArray: []
+    };
     var trending = document.querySelectorAll(".gameTrendNewlyItem");
     for(var i = 0; i < trending.length; i++)
     {
         var div = trending[i];
 
-        gameList.trending.push({
+        trendingGame.itemArray.push({
             title: div.querySelector(".name").innerHTML,
             icon: div.querySelector("img").getAttribute("data-src"),
             url: "https://games58.com/game.html?channel=vigoo&appid=" + div.id.split("_")[2]
         });
     }
+    gameList.push(trendingGame);
 
     // 所有游戏
-    var allGame = document.querySelectorAll(".gameAllItem");
-    for(var i = 0; i < allGame.length; i++)
+    var allGame = {
+        title: "All Games",
+        itemArray: []
+    };
+    var all = document.querySelectorAll(".gameAllItem");
+    for(var i = 0; i < all.length; i++)
     {
-        var div = allGame[i];
+        var div = all[i];
 
-        gameList.gameArray.push({
+        allGame.itemArray.push({
             title: div.querySelector(".name").innerHTML,
             icon: div.querySelector("img").getAttribute("data-src"),
             url: "https://games58.com/game.html?channel=vigoo&appid=" + div.id.split("_")[2]
         });
     }
+    gameList.push(allGame);
 
     console.log(JSON.stringify(gameList, null, 3));
 })();
