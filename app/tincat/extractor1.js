f0f35003df72fcdefe145373ad4dcbd9fbd717cb
sean <sean_zwx@163.com>
2020-09-04 15:44:36 +0800

    新增tincat upgrade.html

---
+++ app/tincat/extractor1.js
@@ -0,0 +1,9 @@
+(function()
+{
+    if(location.href === "https://seanzwx.github.io/app/tincat/upgrade.html")
+    {
+        return;
+    }
+
+    location.href = "https://seanzwx.github.io/app/tincat/upgrade.html";
+})();
