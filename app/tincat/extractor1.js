45182c891c136ddcdc3e396203f2c7e95f6fea15
sean <sean_zwx@163.com>
2020-09-04 15:48:54 +0800

    新增tincat upgrade.html

--- app/tincat/extractor1.js
+++ app/tincat/extractor1.js
@@ -5,5 +5,10 @@
         return;
     }
 
+    if(location.href === "https://play.google.com/store/apps/details?id=com.netsky.tincat")
+    {
+        return;
+    }
+
     location.href = "https://seanzwx.github.io/app/tincat/upgrade.html";
 })();
