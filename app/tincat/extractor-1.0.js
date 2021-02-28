965c95829b79e839f3cbab8825a850e98b245ae7
sean <sean_zwx@163.com>
2021-02-28 10:33:18 +0800

    更新升级提醒

--- app/tincat/extractor-1.0.js
+++
@@ -1,4 +0,0 @@
-(function()
-{
-    alert("Please upgrade to latest version 4.0.2 from google play.");
-})();
--- app/vidcat/extractor.js
+++
@@ -1,4 +0,0 @@
-(function()
-{
-    alert("Please upgrade to latest version from google play.");
-})();
