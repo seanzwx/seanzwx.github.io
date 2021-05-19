68d1b700a77f8b94c21d9a17b1d31bce2fd04993
sean <sean_zwx@163.com>
2021-05-19 15:55:12 +0800

    修改adblock

--- app/tincat/tincat-1.2.js
+++ app/tincat/tincat-1.2.js
@@ -13,12 +13,29 @@
         var defRules = [
             "iframe[src*='pos.baidu.com']"
         ];
+        var siteRules = {
+            ".baidu.com": [".top-ad-beneath-title", ".detail-bottom-ad"]
+        };
 
         var classes = "";
         for(var i = 0; i < defRules.length; i++)
         {
             classes += defRules[i] + "{display:none !important}";
         }
+        var host = location.hostname;
+        for(var key in siteRules)
+        {
+            if(host.indexOf(key) > -1)
+            {
+                var rules = siteRules[key];
+                for(var i = 0; i < rules.length; i++)
+                {
+                    classes += rules[i] + "{display:none !important}";
+                }
+            }
+        }
+
+        console.log(classes);
         var style = document.createElement("style");
         style.innerHTML = classes;
         document.getElementsByTagName("HEAD").item(0).appendChild(style);
