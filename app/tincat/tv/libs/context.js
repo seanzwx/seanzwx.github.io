45b1f1c5fe78fd92be088572bf2d1f93e26795ff
sean <sean_zwx@163.com>
2021-07-27 17:43:27 +0800

    Merge branch 'master' of https://github.com/seanzwx/seanzwx.github.io # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ app/tincat/tv/libs/context.js
@@ -0,0 +1,7 @@
+var Gateway = "http://47.241.101.128";
+
+window.Api = {
+    tincat_tv_channelList_v1: Gateway + "/tincat/tv/channelList/v1",
+    tincat_tv_addChannel_v1: Gateway + "/tincat/tv/addChannel/v1",
+    tincat_tv_deleteChannel_v1: Gateway + "/tincat/tv/deleteChannel/v1"
+};
