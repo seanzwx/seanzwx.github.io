888e6f9e1dd9ffccf318ea42660d04a9ffbb5833
sean <sean_zwx@163.com>
2021-08-16 10:58:08 +0800

    新增vidcat.js

--- app/vidcat/app/js/vidcat-1.2.js
+++ app/vidcat/app/js/vidcat-1.2.js
@@ -1,7 +1,44 @@
 (function()
 {
+    var enableAdBlocker = "${EnableAdBlocker}";
+
     if(!window.vidcat)
     {
         window.vidcat = {};
+    }
+
+    /** 开启adblocker **/
+    if("true" === enableAdBlocker)
+    {
+        var defRules = [
+            "iframe[src*='pos.baidu.com']",
+            "iframe[src*='amazon-adsystem.com']"
+        ];
+        var siteRules = {
+            ".baidu.com": [".fengchao-banner", ".top-ad-beneath-title", ".detail-bottom-ad", ".n-item-ad", ".ad-item"]
+        };
+
+        var classes = "";
+        for(var i = 0; i < defRules.length; i++)
+        {
+            classes += defRules[i] + "{display:none !important}";
+        }
+        var host = location.hostname;
+        for(var key in siteRules)
+        {
+            if(host.indexOf(key) > -1)
+            {
+                var rules = siteRules[key];
+                for(var i = 0; i < rules.length; i++)
+                {
+                    classes += rules[i] + "{display:none !important}";
+                }
+            }
+        }
+
+        console.log(classes);
+        var style = document.createElement("style");
+        style.innerHTML = classes;
+        document.getElementsByTagName("HEAD").item(0).appendChild(style);
     }
 })();
