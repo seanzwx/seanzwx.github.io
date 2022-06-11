782bf618bc09f6ebf6278f5ce16a103e24aab2ad
wishz <zwish163@gmail>
2022-06-11 12:57:13 +0800

    提交movie

---
+++ app/tincat/movie/libs/context.js
@@ -0,0 +1,7 @@
+var Gateway = "https://api.netsky123.com";
+
+window.Api = {
+	tincat_movie: Gateway + "/tincat/v1/movie/",
+	tincat_movie_home: Gateway + "/tincat/v1/movie/home",
+	tincat_movie_download: Gateway + "/tincat/v1/movie/download",
+};
--- app/tincat/tincat-1.1.js
+++
@@ -1,4 +0,0 @@
-(function()
-{
-    alert("please upgrade to latest version from google play store.");
-})();
