e43ffea2e793229fa543fa060003189a6ab13f1b
wishz <zwish163@gmail>
2023-12-23 19:36:29 +0800

    升级adblock

--- app/tincat/movie/libs/context.js
+++
@@ -1,6 +0,0 @@
-var Gateway = "https://api.netsky123.com";
-
-window.Api = {
-	tincat_movie: Gateway + "/tincat/v2/movie/",
-	tincat_movie_home: Gateway + "/tincat/v2/movie/home"
-};
--- app/tincat/shop/libs/context.js
+++
@@ -1,5 +0,0 @@
-var Gateway = "http://47.241.101.128";
-
-window.Api = {
-    shop_app_home_v1: Gateway + "/shop/app/home/v1",
-};
